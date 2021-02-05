ec20bf665b0da0ec4367c15c82d032ad2d5bfa29
oen142 <oen142@naver.com>
2021-02-05 19:53:17 +0900

    Initial commit

---
+++ src/main/java/com/wani/infrasubwayatdd/InfraSubwayAtddApplication.java
@@ -0,0 +1,13 @@
+package com.wani.infrasubwayatdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraSubwayAtddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraSubwayAtddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wani/infrasubwayatdd/InfraSubwayAtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wani.infrasubwayatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraSubwayAtddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
